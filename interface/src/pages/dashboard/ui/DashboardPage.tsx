4a96fc79fa61a3676da6d7c3d9b1cf3442c3059f
Joao Favoretti <joaopedrofavoretti@gmail.com>
2025-11-07 22:35:03 -0300

    Adding toggable resizable sidebar

--- interface/src/pages/dashboard/ui/DashboardPage.tsx
+++ interface/src/pages/dashboard/ui/DashboardPage.tsx
@@ -1,8 +1,12 @@
 import { Header } from "./Header";
-import { Box } from "@mui/material";
+import { useState, useRef } from "react";
+import { Box, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import { MapViewer } from "./MapViewer";
 import { SidebarPanel } from "./SidebarPanel";
 import { MapProvider } from "@/shared/lib/map";
+import type { ImperativePanelHandle } from "react-resizable-panels";
 import { StripsProvider } from "@/shared/lib/strips";
 import {
   ResizablePanelGroup,
@@ -11,6 +15,9 @@ import {
 } from "@/shared/ui";
 
 export const DashboardPage = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+  const sidebarRef = useRef<ImperativePanelHandle>(null);
+
   return (
     <MapProvider>
       <StripsProvider>
@@ -23,22 +30,60 @@ export const DashboardPage = () => {
           }}
         >
           <Header />
-          <ResizablePanelGroup direction="horizontal" className="flex-1">
-            <ResizablePanel defaultSize={30} minSize={20} maxSize={40}>
+
+          <ResizablePanelGroup
+            direction="horizontal"
+            className="flex-1"
+            autoSaveId="dashboard-panels"
+          >
+            <ResizablePanel
+              id="sidebar"
+              ref={sidebarRef}
+              defaultSize={20}
+              minSize={0}
+              maxSize={40}
+              collapsedSize={0}
+              collapsible
+              onCollapse={() => setSidebarCollapsed(true)}
+              onExpand={() => setSidebarCollapsed(false)}
+            >
               <SidebarPanel />
             </ResizablePanel>
             <ResizableHandle withHandle />
-            <ResizablePanel defaultSize={70}>
-              <Box
-                component="main"
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MapViewer />
+            <ResizablePanel id="main" defaultSize={80} minSize={40}>
+              <Box sx={{ height: "100%", position: "relative" }}>
+                <IconButton
+                  onClick={() => {
+                    if (sidebarCollapsed) {
+                      sidebarRef.current?.expand();
+                    } else {
+                      sidebarRef.current?.collapse();
+                    }
+                  }}
+                  sx={{
+                    position: "absolute",
+                    top: 8,
+                    left: 8,
+                    zIndex: 10,
+                    bgcolor: "background.paper",
+                    border: 1,
+                    borderColor: "divider",
+                    "&:hover": { bgcolor: "action.hover" },
+                  }}
+                >
+                  {sidebarCollapsed ? <MenuIcon /> : <MenuOpenIcon />}
+                </IconButton>
+                <Box
+                  component="main"
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <MapViewer />
+                </Box>
               </Box>
             </ResizablePanel>
           </ResizablePanelGroup>
